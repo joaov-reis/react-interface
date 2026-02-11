5b000f88eb5254a2e8f8e897c4dd6de1d3a8ee22
reisjoaov <joaoreisvictor@hotmail.com>
2026-02-10 22:57:16 -0300

    Criados primeiros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Olá</p>
+      <Welcome />
+      <Button
+        title="Clique aqui"
+        // isDisabled
+        onClick={() => {
+          alert("Botão clicado!!!");
+          console.log("Mensagem no console");
+        }}
+        version={1}
+        // size={2}
+        // obj={{
+        //   chave: "valor",
+        // }}
+        // lista={[1, 2, 3]}
+      />
+
+      <Button
+        // title="Clique aqui"
+        isDisabled
+        onClick={() => {}}
+      />
+
+      <Card title="Titulo do card">
+        <div>Corpo do card</div>
+      </Card>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const Welcome: React.FC = () => {
+  const mensagem = "Olá, mundo!";
+  const horaAtual = new Date().toLocaleTimeString();
+
+  return (
+    <div>
+      <h1>
+        {mensagem}
+        <br /> Agora são {horaAtual}
+      </h1>
+      <p>{10 + 5}</p>
+    </div>
+  );
+};
+
+interface ButtonProps {
+  title?: string;
+  isDisabled?: boolean;
+  onClick: () => void;
+  version?: 1 | 2 | "3";
+}
+
+function Button({
+  onClick,
+  title = "Titulo padrão",
+  isDisabled = false,
+}: ButtonProps) {
+  return (
+    <button disabled={isDisabled} onClick={onClick}>
+      {title}
+    </button>
+  );
+}
+
+interface CardProps {
+  title: string;
+  children?: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ children, title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>{children}</div>
+    </div>
+  );
+};
