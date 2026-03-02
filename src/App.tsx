12a33c6375fa6b85db886484bc8d7704f717fe60
reisjoaov <joaoreisvictor@hotmail.com>
2026-03-01 21:02:05 -0300

    Ajustes no Git

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,32 @@
 import { useMemo, useState } from "react";
 import "./App.css";
-import Examples from "./components/example-flexbox";
-import Avatar from "./components/avatar";
-import { useEffect, useState } from "react";
-
-type Count = number;
-type Name = string;
-type Show = boolean;
 
 function App() {
-  const [count, setCount] = useState<Count>(0);
-  const [name, setName] = useState<Name>("");
-  const [show, setShow] = useState<Show>(false);
-  const [scrollY, setScrollY] = useState<Number>(0);
+  const [tasks, setTasks] = useState<string[]>([
+    "Estudar React",
+    "Lavar a louça",
+    "Varrer a casa",
+  ]);
 
-  // console.log("Componente executado");
+  const [searchText, setSearchText] = useState("");
 
-  // useEffect(() => {
-  //   console.log("Componente construido");
-  // }, [name]);
-
-  // useEffect(() => {
-  //   if (count % 2 === 0) {
-  //     setShow((prev) => !prev);
-  //   }
-  // }, [count]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      console.log(window.scrollY);
-      setScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const filteredTasks = useMemo(
+    () =>
+      tasks.filter((task) => {
+        console.log("Processando tarefa: ", task);
+        return task.toLowerCase().includes(searchText.toLowerCase());
+      }),
+    [searchText, tasks],
+  );
 
   return (
     <>
-      {show && <ShowCount value={count} />}
-      <ShowCount value={scrollY} />
-      <Button text="Clique aqui" onClick={() => setCount(count! + 1)} />
-      <Input
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-        value={name}
-      />
-    </>
-  );
-}
-
-interface ButtonProps {
-  text?: string;
-  onClick: () => void;
-}
-
-function ShowCount({ value: text }: { value: number }) {
-  useEffect(() => {
-    console.log("Componente montado");
-    return () => console.log("Componente desmontado");
-  });
-  return <div>{<h1>{text}</h1>}</div>;
-}
-
-function Button({ onClick, text }: ButtonProps) {
-  return (
-    <button
-      onClick={onClick}
-      // onDoubleClick={() => {
-      //   console.log("Duplo clique!");
-      // }}
-      // onMouseEnter={() => {
-      //   console.log("Mouse em cima do botao!");
-      // }}
-      // onMouseLeave={() => {
-      //   console.log("Mouse saiu do botao!");
-      // }}
-    >
-      {text}
-    </button>
-  );
-}
-
-function Input({ onChange, value }: { onChange: any; value: string }) {
-  return (
-    <input
-      type="text"
-      placeholder="Digite seu texto"
-      name="nome"
-      onChange={onChange}
-      value={value}
-      onKeyDown={({ key }) => {
-        // console.log(key)
-        // if(key === "Enter"){
-        //   alert("Fomulário enviado")
-        // }
-      }}
-      // onKeyUp={(event) => {
-      //   console.log(event.key);
-      // }}
-      // onBlur={(event) => {
-      //   console.log(event, "Desfocado");
-      // }}
-      // onFocus={(event) => {
-      //   console.log(event, "Elemento focado");
-      // }}
-    />
-  );
-}
-
-function App_() {
-  return (
-    <div>
-      <Card title="Perfil">
-        <Avatar
-          name="infnet2"
-          imageUrl="https://cdn-icons-png.flaticon.com/512/147/147142.png"
+      <div>
+        <input
+          type="text"
+          value={searchText}
+          onChange={({ target: { value } }) => setSearchText(value)}
+          placeholder="Pesquisar tarefa"
         />
       </div>
       <ul>
@@ -129,4 +39,3 @@ function App_() {
 }
 
 export default App;
-<div>Olá</div>;
